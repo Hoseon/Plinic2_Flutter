008df4420ec01050d37dae8460e3697b921d40dc
Leesungkyun <g1plinc@gmail.com>
2021-12-20 08:49:12 +0900

    정기결제 테스트 개발 완료_20211220(근무시작)

--- lib/src/pages/subscribe/subscribe_regist_cart.dart
+++ lib/src/pages/subscribe/subscribe_regist_cart.dart
@@ -19,165 +19,223 @@ class SubScribeRegistCard extends GetView<CreditCardController> {
   final TextEditingController _controller3 = TextEditingController();
   final TextEditingController _controller4 = TextEditingController();
 
+  Map<String, dynamic>? _data;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: CustomAppbar('카드 등록'),
         backgroundColor: white,
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: spacing_xxl),
-              /////////////
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.symmetric(horizontal: spacing_xl),
-                child: Text(
-                  '결제수단을 등록해주세요.',
-                  style: TextStyle(
-                    color: grey_1,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    fontStyle: FontStyle.normal,
+        body: GestureDetector(
+          onTap: () {
+            Get.focusScope!.unfocus();
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: spacing_xxl),
+                /////////////
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.symmetric(horizontal: spacing_xl),
+                  child: Text(
+                    '결제수단을 등록해주세요.',
+                    style: TextStyle(
+                      color: grey_1,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                    ),
                   ),
                 ),
-              ),
-              //////////////
-              Obx(
-                () => CreditCardWidget(
-                  cardNumber: controller.cardNumber.value,
-                  expiryDate: controller.expireDate.value,
-                  cardHolderName: 'Plinic Text',
-                  cvvCode: '',
-                  showBackView: false,
-                  onCreditCardWidgetChange: (value) {
-                    print(value);
-                  },
-                  cardBgColor: primary_dark,
-                ),
-              ),
-              SizedBox(height: spacing_xs),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: spacing_xl),
-                child: Container(
-                  width: Get.mediaQuery.size.width,
-                  height: 304,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.red, width: 0.5),
+                //////////////
+                Obx(
+                  () => CreditCardWidget(
+                    cardNumber: controller.cardNumber.value,
+                    expiryDate: controller.expireDate.value,
+                    cardHolderName: 'Plinic Text',
+                    cvvCode: '',
+                    showBackView: false,
+                    onCreditCardWidgetChange: (value) {
+                      print(value);
+                    },
+                    cardBgColor: primary_dark,
                   ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: spacing_s),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.symmetric(horizontal: 15),
-                        child: Text(
-                          '카드번호',
+                ),
+                SizedBox(height: spacing_xs),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: spacing_xl),
+                  child: Container(
+                    width: Get.mediaQuery.size.width,
+                    height: 304,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 0.5),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(height: spacing_s),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.symmetric(horizontal: 15),
+                          child: Text(
+                            '카드번호',
+                            style: TextStyle(
+                              color: black,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                        ),
+                        Form(
+                          key: _formKey1,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  controller: _controller1,
+                                  validator: (String? value) {
+                                    if (value!.length == 16 || value.isEmpty) {
+                                      return '카드 정보를 정확하게 입력하세요';
+                                    }
+                                    return null;
+                                  },
+                                  onChanged: (value) {
+                                    controller
+                                        .updateCardNumber(_controller1.text);
+                                  },
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: [
+                                    CreditCardNumberInputFormatter(),
+                                    // MaskedInputFormatter('################')
+                                  ],
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'XXXX XXXX XXXX XXXX',
+                                  ),
+                                ),
+                                SizedBox(height: spacing_xs),
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    '카드 만료일(월/년도)',
+                                    style: TextStyle(
+                                      color: black,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                    ),
+                                  ),
+                                ),
+                                TextFormField(
+                                  controller: _controller2,
+                                  onChanged: (value) {
+                                    controller.updateExpireDate(value);
+                                  },
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: [
+                                    CreditCardExpirationDateFormatter()
+                                  ],
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'ex)01/26',
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    '카드 소유주 생년월일 혹은 법인 번호',
+                                    style: TextStyle(
+                                      color: black,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                    ),
+                                  ),
+                                ),
+                                TextFormField(
+                                  controller: _controller3,
+                                  onChanged: (value) {
+                                    controller.updateIdentifyNumber(value);
+                                  },
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: [
+                                    MaskedInputFormatter('######')
+                                  ],
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'ex)841025',
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    '카드 앞 2자리 비밀번호',
+                                    style: TextStyle(
+                                      color: black,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                    ),
+                                  ),
+                                ),
+                                TextFormField(
+                                  controller: _controller4,
+                                  onChanged: (value) {
+                                    controller.updatePwFrton(value);
+                                  },
+                                  keyboardType: TextInputType.number,
+                                  obscureText: true,
+                                  inputFormatters: [MaskedInputFormatter('00')],
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: '00',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: spacing_xxl),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: spacing_xl),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints.tightFor(
+                        width: Get.mediaQuery.size.width, height: 42),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        elevation: MaterialStateProperty.all(0.0),
+                        backgroundColor:
+                            MaterialStateProperty.all(primaryColor),
+                      ),
+                      onPressed: () {
+                        _data = {
+                          'cardNo': controller.cardNumber.value,
+                          'cardPw': controller.pwFront.value,
+                          'expireYear': controller.expireYear.value,
+                          'expireMonth': controller.expireMonth.value,
+                          'identifyNumber': controller.identifyNumber.value,
+                        };
+                        controller.createBill(_data);
+                      },
+                      child: Text('카드 정보 등록',
                           style: TextStyle(
-                            color: black,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w400,
+                            fontFamily: 'NotoSansKR',
+                            color: Color(0xffffffff),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
                             fontStyle: FontStyle.normal,
-                          ),
-                        ),
-                      ),
-                      Form(
-                        key: _formKey1,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: Column(
-                            children: [
-                              TextFormField(
-                                controller: _controller1,
-                                validator: (String? value) {
-                                  if (value!.length == 16 || value.isEmpty) {
-                                    return '카드 정보를 정확하게 입력하세요';
-                                  }
-                                  return null;
-                                },
-                                keyboardType: TextInputType.number,
-                                inputFormatters: [
-                                  CreditCardNumberInputFormatter(),
-                                ],
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'XXXX XXXX XXXX XXXX',
-                                ),
-                              ),
-                              SizedBox(height: spacing_xs),
-                              Container(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  '카드 만료일(월/년도)',
-                                  style: TextStyle(
-                                    color: black,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                    fontStyle: FontStyle.normal,
-                                  ),
-                                ),
-                              ),
-                              TextFormField(
-                                keyboardType: TextInputType.number,
-                                inputFormatters: [
-                                  CreditCardExpirationDateFormatter()
-                                ],
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'ex)01/26',
-                                ),
-                              ),
-                              Container(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  '카드 소유주 생년월일 혹은 법인 번호',
-                                  style: TextStyle(
-                                    color: black,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                    fontStyle: FontStyle.normal,
-                                  ),
-                                ),
-                              ),
-                              TextFormField(
-                                keyboardType: TextInputType.number,
-                                inputFormatters: [
-                                  MaskedInputFormatter('######')
-                                ],
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'ex)841025',
-                                ),
-                              ),
-                              Container(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  '카드 앞 2자리 비밀번호',
-                                  style: TextStyle(
-                                    color: black,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                    fontStyle: FontStyle.normal,
-                                  ),
-                                ),
-                              ),
-                              TextFormField(
-                                keyboardType: TextInputType.number,
-                                inputFormatters: [MaskedInputFormatter('00')],
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: '00',
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+                          )),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ));
   }
--- lib/src/repository/bill_repository.dart
+++ lib/src/repository/bill_repository.dart
@@ -1,15 +1,44 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
-class billRepository extends GetxService {
+class BillRepository extends GetxService {
   Future<bool> billCreate(Map<String, dynamic> data) async {
-    var url = Uri.parse('https://example.com/whatsit/create');
-    var response =
-        await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+    print(data);
+    // print(data['cardNo'].toString());
+    // print(data['cardPw'].toString());
+    // print(data['expireYear'].toString());
+    // print(data['expireMonth'].toString());
+    // print(data['identifyNumber'].toString());
+    // var formData = dio.FormData.fromMap({
+    //   'cardNo': data['cardNo'].toString(),
+    //   'cardPw': data['cardPw'].toString(),
+    //   'expireYear': data['expireYear'].toString(),
+    //   'expireMonth': data['expireMonth'].toString(),
+    //   'identifyNumber': data['identifyNumber'].toString(),
+    // });
+    // var dio2 = dio.Dio();
+
+    // try {
+    //   var response = await dio2.get('http://www.google.com');
+    //   print(response);
+    // } catch (e) {
+    //   print(e);
+    // }
+    // var response =
+    //     await dio2.post('https://admin.g1p.xyz/api/createBill', data: formData);
+    // print(response);
+
+    var dio = Dio(BaseOptions(baseUrl: 'https://admin.g1p.xyz/api'));
+
+    var response = await dio.post('/createBill', data: {
+      'cardNo': data['cardNo'].toString(),
+      'cardPw': data['cardPw'].toString(),
+      'expireYear': data['expireYear'].toString(),
+      'expireMonth': data['expireMonth'].toString(),
+      'identifyNumber': data['identifyNumber'].toString(),
+    });
+    print(response.data);
     return true;
   }
 }
